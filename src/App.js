952c71488499196cdd4e9562d361faffff78bfdd
Elina Pohrebna <67369830+elinapohrebna@users.noreply.github.com>
2021-10-12 16:13:08 +0300

    Added material ui configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './styles/theme';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <div>
+      <p> Movie Knight</p>
     </div>
+  </ThemeProvider>
   );
 }
 
